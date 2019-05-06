5f853c2607ed9a7ec6498d47f43a534030b987ee
unow <965545403@qq.com>
2019-05-06 21:18:37 +0800

    统一请求响应体

--- seckill/src/main/java/top/unow/seckill/result/CodeMsg.java
+++ seckill/src/main/java/top/unow/seckill/result/CodeMsg.java
@@ -13,8 +13,9 @@ public class CodeMsg {
     private int code;
     private String msg;
 
-    //通用的错误码
     public static CodeMsg SUCCESS = new CodeMsg(0, "success");
+
+    //通用的错误码
     public static CodeMsg SERVER_ERROR = new CodeMsg(500100, "服务端异常");
     public static CodeMsg BIND_ERROR = new CodeMsg(500101, "参数校验异常：%s");
     public static CodeMsg ACCESS_LIMIT_REACHED= new CodeMsg(500104, "访问高峰期，请稍等！");
